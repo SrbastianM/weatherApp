796a92d749801d6227c3fc7c934dc835235de096
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-07-04 00:44:34 -0500

    Set the FirstPageActivity and implement new dependencies

--- app/src/main/java/com/srbastian/weatherapp/view/FirstPageActivity.kt
+++ app/src/main/java/com/srbastian/weatherapp/view/FirstPageActivity.kt
@@ -3,10 +3,16 @@ package com.srbastian.weatherapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.srbastian.weatherapp.R
+import com.srbastian.weatherapp.databinding.ActivityFirstPageBinding
 
 class FirstPageActivity : AppCompatActivity() {
+    lateinit var firstPageBinding: ActivityFirstPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_page)
+        firstPageBinding = ActivityFirstPageBinding.inflate(layoutInflater)
+        val view = firstPageBinding.root
+        setContentView(view)
+
+
     }
 }
