31f321ccde60b65875546c13d9b219753fb4e655
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-07-07 11:58:51 -0500

    Although I couldn't migrate the project to Jetpack Compose. - Design: I finish the design of the FistPage, so I'm going to make it functional.

--- app/src/main/java/com/srbastian/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/srbastian/weatherapp/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.savedstate.SavedStateRegistryOwner
